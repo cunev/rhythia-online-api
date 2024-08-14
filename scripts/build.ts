aef999b0afd37d9f5ac01139c8347ae9f0e7904b
cunev <cunevnew@gmail.com>
2024-08-14 14:19:46 -0700

    feat: build to zip

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,16 +1,35 @@
+import { zipFunctions } from "@netlify/zip-it-and-ship-it";
 import { buildSync } from "esbuild";
-import { readdirSync } from "fs";
+import { readdirSync, rmSync } from "fs";
 import path from "path";
 
 const apis = readdirSync("./api");
 
+// Build
 for (const api of apis) {
   const filePath = path.join("./api", api);
-  buildSync({
-    entryPoints: [filePath],
-    outdir: "./dist",
-    platform: "node",
-    format: "cjs",
-    bundle: true,
-  });
+  try {
+    buildSync({
+      entryPoints: [filePath],
+      outfile: `./dist/${path.parse(filePath).name}/index.js`,
+      platform: "node",
+      format: "cjs",
+      bundle: true,
+    });
+  } catch (error) {
+    console.log(`SKIPPABLE ERROR: Can't build api ${filePath}`);
+  }
 }
+
+// Zip
+zipFunctions("dist", "dist", {
+  archiveFormat: "zip",
+}).then((e) => {
+  const outs = readdirSync("./dist");
+  for (const outfile of outs) {
+    const filepath = path.join("./dist", outfile);
+    if (!filepath.endsWith(".zip")) {
+      rmSync(filepath, { recursive: true });
+    }
+  }
+});
