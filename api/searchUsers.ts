675a7516e1f040d588ea6e047784bfbe2081863e
cunev <cunevnew@gmail.com>
2024-08-18 12:59:57 -0700

    feat: search api

---
+++ api/searchUsers.ts
@@ -0,0 +1,46 @@
+import { NextResponse } from "next/server";
+import z from "zod";
+import { protectedApi } from "../utils/requestUtils";
+import { supabase } from "../utils/supabase";
+
+export const Schema = {
+  input: z.strictObject({
+    text: z.string(),
+  }),
+  output: z.object({
+    error: z.string().optional(),
+    results: z
+      .array(
+        z.object({
+          id: z.number(),
+          username: z.string().nullable(),
+        })
+      )
+      .optional(),
+  }),
+};
+
+export async function POST(res: Response): Promise<NextResponse> {
+  return protectedApi({
+    response: res,
+    schema: Schema,
+    authorization: () => {},
+    activity: handler,
+  });
+}
+
+export async function handler(
+  data: (typeof Schema)["input"]["_type"]
+): Promise<NextResponse<(typeof Schema)["output"]["_type"]>> {
+  const { data: searchData, error } = await supabase
+    .from("profiles")
+    .select()
+    .ilike("username", `%${data.text}%`)
+    .limit(10);
+  return NextResponse.json({
+    results: (searchData || []).map((data) => ({
+      id: data.id,
+      username: data.username,
+    })),
+  });
+}
