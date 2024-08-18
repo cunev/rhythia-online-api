56f606dc646b9c2e39f92506e0ceac415b138459
cunev <cunevnew@gmail.com>
2024-08-18 03:07:18 -0700

    feat: update-user

--- api/editProfile.ts
+++ api/editProfile.ts
@@ -1,10 +1,17 @@
 import { NextResponse } from "next/server";
 import z from "zod";
+import { Database } from "../types/database";
 import { protectedApi, validUser } from "../utils/requestUtils";
+import { supabase } from "../utils/supabase";
 
 export const Schema = {
   input: z.object({
     session: z.string(),
+    data: z.object({
+      avatar_url: z.string().optional(),
+      about_me: z.string().optional(),
+      username: z.string().optional(),
+    }),
   }),
   output: z.object({
     error: z.string().optional(),
@@ -23,5 +30,18 @@ export async function POST(res: Response): Promise<NextResponse> {
 async function handler(
   data: (typeof Schema)["input"]["_type"]
 ): Promise<NextResponse<(typeof Schema)["output"]["_type"]>> {
+  const user = (await supabase.auth.getUser(data.session)).data.user!;
+
+  const upsertPayload: Database["public"]["Tables"]["profiles"]["Update"] = {
+    uid: user.id,
+    flag: "",
+    ...data.data,
+  };
+
+  const upsertResult = await supabase
+    .from("profiles")
+    .upsert(upsertPayload)
+    .select();
+
   return NextResponse.json({});
 }
