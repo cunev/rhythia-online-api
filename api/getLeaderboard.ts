02bc36454867c00c56a23c73e91db261693c39e0
cunev <cunevnew@gmail.com>
2024-08-21 13:22:27 -0700

    feat: remove verify

--- api/getLeaderboard.ts
+++ api/getLeaderboard.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import z from "zod";
-import { protectedApi, validUser } from "../utils/requestUtils";
+import { protectedApi } from "../utils/requestUtils";
 import { supabase } from "../utils/supabase";
 
 export const Schema = {
@@ -32,7 +32,7 @@ export async function POST(request: Request): Promise<NextResponse> {
   return protectedApi({
     request,
     schema: Schema,
-    authorization: validUser,
+    authorization: () => {},
     activity: handler,
   });
 }
