972d471ccd13195bec8d688636ae27bc9319cdf6
cunev <cunevnew@gmail.com>
2024-09-02 11:47:18 -0700

    feat: upload img url callback

---
+++ api/getAvatarUploadUrl.ts
@@ -0,0 +1,32 @@
+import { NextResponse } from "next/server";
+import z from "zod";
+import { protectedApi, validUser } from "../utils/requestUtils";
+import { supabase } from "../utils/supabase";
+
+export const Schema = {
+  input: z.strictObject({
+    session: z.string(),
+  }),
+  output: z.object({
+    url: z.string(),
+  }),
+};
+
+export async function POST(request: Request): Promise<NextResponse> {
+  return protectedApi({
+    request,
+    schema: Schema,
+    authorization: validUser,
+    activity: handler,
+  });
+}
+
+export async function handler({
+  session,
+}: (typeof Schema)["input"]["_type"]): Promise<
+  NextResponse<(typeof Schema)["output"]["_type"]>
+> {
+  const user = (await supabase.auth.getUser(session)).data.user!;
+
+  return NextResponse.json({ url: "" });
+}
