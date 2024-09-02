d9402e30f6bab689504712587647f869e97a88c6
cunev <cunevnew@gmail.com>
2024-09-02 12:01:02 -0700

    feat: getAvatarUploadUrl

--- api/getAvatarUploadUrl.ts
+++ api/getAvatarUploadUrl.ts
@@ -3,6 +3,17 @@ import z from "zod";
 import { protectedApi, validUser } from "../utils/requestUtils";
 import { supabase } from "../utils/supabase";
 
+import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+
+const s3Client = new S3Client({
+  region: "us-east-1",
+  credentials: {
+    secretAccessKey: "0036dd2cb06d730015861d27ce0796cccb5031123e",
+    accessKeyId: "c245e893e806",
+  },
+});
+
 export const Schema = {
   input: z.strictObject({
     session: z.string(),
@@ -28,5 +39,17 @@ export async function handler({
 > {
   const user = (await supabase.auth.getUser(session)).data.user!;
 
-  return NextResponse.json({ url: "" });
+  const command = new PutObjectCommand({
+    Bucket: "rhythia-avatars",
+    Key: `user-avatar-${Date.now()}-${user.id}`,
+    ContentLength: 5000000,
+  });
+
+  const presigned = await getSignedUrl(s3Client, command, {
+    expiresIn: 3600,
+  });
+  return NextResponse.json({
+    url: presigned,
+    objectKey: `user-avatar-${Date.now()}-${user.id}`,
+  });
 }
