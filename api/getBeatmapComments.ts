d60fbe5f084879814afa75adff89f0c5386868d7
cunev <cunevnew@gmail.com>
2024-10-13 12:33:02 -0700

    feat: getBeatmapComments

---
+++ api/getBeatmapComments.ts
@@ -0,0 +1,42 @@
+import { NextResponse } from "next/server";
+import z from "zod";
+import { protectedApi, validUser } from "../utils/requestUtils";
+import { supabase } from "../utils/supabase";
+
+export const Schema = {
+  input: z.strictObject({
+    page: z.number(),
+  }),
+  output: z.strictObject({
+    error: z.string().optional(),
+    comments: z.array(
+      z.object({
+        beatmapPage: z.number(),
+        content: z.string().nullable(),
+        owner: z.number(),
+      })
+    ),
+  }),
+};
+
+export async function POST(request: Request): Promise<NextResponse> {
+  return protectedApi({
+    request,
+    schema: Schema,
+    authorization: () => {},
+    activity: handler,
+  });
+}
+
+export async function handler({
+  page,
+}: (typeof Schema)["input"]["_type"]): Promise<
+  NextResponse<(typeof Schema)["output"]["_type"]>
+> {
+  let { data: userData, error: userError } = await supabase
+    .from("beatmapPageComments")
+    .select("*")
+    .eq("beatmapPage", page);
+
+  return NextResponse.json({ comments: userData! });
+}
