97557e19f125294000a225f0c9071cf208187137
cunev <cunevnew@gmail.com>
2024-10-13 04:39:52 -0700

    feat: forceRank API

---
+++ api/forceRank.ts
@@ -0,0 +1,60 @@
+import { NextResponse } from "next/server";
+import z from "zod";
+import { protectedApi, validUser } from "../utils/requestUtils";
+import { supabase } from "../utils/supabase";
+
+export const Schema = {
+  input: z.strictObject({
+    session: z.string(),
+    mapId: z.number(),
+  }),
+  output: z.object({
+    error: z.string().optional(),
+  }),
+};
+
+export async function POST(request: Request) {
+  return protectedApi({
+    request,
+    schema: Schema,
+    authorization: validUser,
+    activity: handler,
+  });
+}
+
+export async function handler(data: (typeof Schema)["input"]["_type"]) {
+  const user = (await supabase.auth.getUser(data.session)).data.user!;
+  let { data: queryUserData, error: userError } = await supabase
+    .from("profiles")
+    .select("*")
+    .eq("uid", user.id)
+    .single();
+
+  if (!queryUserData) {
+    return NextResponse.json({ error: "Can't find user" });
+  }
+
+  const tags = (queryUserData?.badges || []) as string[];
+
+  if (!tags.includes("Bot")) {
+    return NextResponse.json({ error: "Only Bots can force-rank maps!" });
+  }
+
+  const { data: mapData, error } = await supabase
+    .from("beatmapPages")
+    .select("id,nominations,owner")
+    .eq("id", data.mapId)
+    .single();
+
+  if (!mapData) {
+    return NextResponse.json({ error: "Bad map" });
+  }
+
+  await supabase.from("beatmapPages").upsert({
+    id: data.mapId,
+    nominations: [queryUserData.id, queryUserData.id],
+    status: "RANKED",
+  });
+
+  return NextResponse.json({});
+}
