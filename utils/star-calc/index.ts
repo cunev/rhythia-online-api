eed8c4e89350b2e2bffcd187683403d85413893d
cunev <cunevnew@gmail.com>
2024-10-30 11:09:07 +0200

    feat: change star rating

--- utils/star-calc/index.ts
+++ utils/star-calc/index.ts
@@ -1,4 +1,4 @@
-import { BeatmapDecoder } from "osu-parsers";
+import { BeatmapDecoder, BeatmapEncoder } from "osu-parsers";
 import { StandardRuleset } from "osu-standard-stable";
 import { sampleMap } from "./osuUtils";
 import { SSPMParsedMap } from "./sspmParser";
@@ -18,9 +18,6 @@ export function calculatePerformancePoints(
 }
 
 export function rateMap(map: SSPMParsedMap) {
-  const decoder = new BeatmapDecoder();
-  const beatmap1 = decoder.decodeFromString(sampleMap);
-
   let notes = map.markers
     .filter((marker) => marker.type === 0)
     .map((marker) => ({
@@ -29,44 +26,52 @@ export function rateMap(map: SSPMParsedMap) {
       y: marker.data["field0"].y,
     }));
 
-  notes = notes.sort((a, b) => a.time - b.time);
-
-  for (const note of notes) {
-    const hittable = beatmap1.hitObjects[0].clone();
-    hittable.startX = Math.round((note.x / 2) * 100);
-    hittable.startY = Math.round((note.y / 2) * 100);
-    hittable.startTime = note.time;
-    beatmap1.hitObjects.push(hittable);
-  }
-  const ruleset = new StandardRuleset();
-  const mods = ruleset.createModCombination("RX");
-  const difficultyCalculator = ruleset.createDifficultyCalculator(beatmap1);
-  const difficultyAttributes = difficultyCalculator.calculateWithMods(mods);
-  return difficultyAttributes.starRating;
+  return rate(notes);
 }
 
 export function rateMapOld(map: SSPMMap) {
-  const decoder = new BeatmapDecoder();
-  const beatmap1 = decoder.decodeFromString(sampleMap);
-
   let notes = map.notes.map((marker) => ({
     time: marker.position,
     x: marker.x,
     y: marker.y,
   }));
 
-  notes = notes.sort((a, b) => a.time - b.time);
+  return rate(notes);
+}
 
-  for (const note of notes) {
+export function rate(
+  notes: {
+    time: number;
+    x: number;
+    y: number;
+  }[]
+) {
+  notes = notes.sort((a, b) => a.time - b.time);
+  const decoder = new BeatmapDecoder();
+  const encoder = new BeatmapEncoder();
+  const beatmap1 = decoder.decodeFromString(sampleMap);
+  let i = 0;
+  while (i < notes.length - 1) {
+    const note = notes[i];
+    const nextNote = notes[i + 1];
+    if (distance(note.x, note.y, nextNote.x, nextNote.y) < 1.25) {
+      notes.splice(i + 1, 1);
+      continue;
+    }
+
     const hittable = beatmap1.hitObjects[0].clone();
     hittable.startX = Math.round((note.x / 2) * 100);
     hittable.startY = Math.round((note.y / 2) * 100);
     hittable.startTime = note.time;
     beatmap1.hitObjects.push(hittable);
+    i++;
   }
   const ruleset = new StandardRuleset();
   const mods = ruleset.createModCombination("RX");
   const difficultyCalculator = ruleset.createDifficultyCalculator(beatmap1);
   const difficultyAttributes = difficultyCalculator.calculateWithMods(mods);
+  encoder.encodeToPath("./map.osu", beatmap1);
   return difficultyAttributes.starRating;
 }
+
+const distance = (x1, y1, x2, y2) => Math.hypot(x2 - x1, y2 - y1);
