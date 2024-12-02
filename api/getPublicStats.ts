7938fafec0d53af7bf8b453de50314b861a5df3d
cunev <cunevnew@gmail.com>
2024-12-02 18:58:30 +0200

    feat: last 4 ranked maps

--- api/getPublicStats.ts
+++ api/getPublicStats.ts
@@ -9,6 +9,26 @@ export const Schema = {
     profiles: z.number(),
     beatmaps: z.number(),
     scores: z.number(),
+    lastBeatmaps: z.array(
+      z.object({
+        id: z.number().nullable().optional(),
+        nominations: z.array(z.number()).nullable().optional(),
+        playcount: z.number().nullable().optional(),
+        created_at: z.string().nullable().optional(),
+        difficulty: z.number().nullable().optional(),
+        noteCount: z.number().nullable().optional(),
+        length: z.number().nullable().optional(),
+        title: z.string().nullable().optional(),
+        ranked: z.boolean().nullable().optional(),
+        beatmapFile: z.string().nullable().optional(),
+        image: z.string().nullable().optional(),
+        starRating: z.number().nullable().optional(),
+        owner: z.number().nullable().optional(),
+        ownerUsername: z.string().nullable().optional(),
+        ownerAvatar: z.string().nullable().optional(),
+        status: z.string().nullable().optional(),
+      })
+    ),
   }),
 };
 
@@ -30,6 +50,33 @@ export async function handler(data: (typeof Schema)["input"]["_type"]) {
     .from("beatmaps")
     .select("*", { count: "exact", head: true });
 
+  let { data: beatmapPage, error: errorlast } = await supabase
+    .from("beatmapPages")
+    .select(
+      `
+      *,
+      beatmaps (
+        created_at,
+        playcount,
+        length,
+        ranked,
+        beatmapFile,
+        image,
+        starRating,
+        difficulty,
+        noteCount,
+        title
+      ),
+      profiles (
+        username,
+        avatar_url
+      )
+    `
+    )
+    .eq("ranked", true)
+    .order("created_at", { ascending: false })
+    .limit(4);
+
   const countScoresQuery = await supabase
     .from("scores")
     .select("*", { count: "exact", head: true });
@@ -38,5 +85,23 @@ export async function handler(data: (typeof Schema)["input"]["_type"]) {
     beatmaps: countBeatmapsQuery.count,
     profiles: countProfilesQuery.count,
     scores: countScoresQuery.count,
+    lastBeatmaps: beatmapPage?.map((e) => ({
+      playcount: e.beatmaps?.playcount,
+      created_at: e.created_at,
+      difficulty: e.beatmaps?.difficulty,
+      noteCount: e.beatmaps?.noteCount,
+      length: e.beatmaps?.length,
+      title: e.beatmaps?.title,
+      ranked: e.beatmaps?.ranked,
+      beatmapFile: e.beatmaps?.beatmapFile,
+      image: e.beatmaps?.image,
+      starRating: e.beatmaps?.starRating,
+      owner: e.owner,
+      ownerUsername: e.profiles?.username,
+      ownerAvatar: e.profiles?.avatar_url,
+      id: e.id,
+      status: e.status,
+      nominations: e.nominations as number[],
+    })),
   });
 }
