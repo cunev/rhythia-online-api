9d1176e93672069e748b4605bcb558dd11eb7bfc
cunev <cunevnew@gmail.com>
2024-12-28 15:27:22 +0200

    feat: disable token validation logic

--- utils/validateToken.ts
+++ utils/validateToken.ts
@@ -1,6 +1,7 @@
 export function validateIntrinsicToken(token: string) {
-  if (!process.env.validationCode) return true;
-  const generatedValidationCode = eval(process.env.validationCode);
-  console.log(generatedValidationCode, token);
-  return generatedValidationCode == token;
+  return true;
+  // if (!process.env.validationCode) return true;
+  // const generatedValidationCode = eval(process.env.validationCode);
+  // console.log(generatedValidationCode, token);
+  // return generatedValidationCode == token;
 }
