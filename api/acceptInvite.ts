907c26fed6f3d1fad8b1131dc968568c9944271b
cunev <cunevnew@gmail.com>
2025-04-16 23:42:30 +0300

    HND/

--- api/acceptInvite.ts
+++ api/acceptInvite.ts
@@ -27,53 +27,5 @@ export async function POST(request: Request) {
 }
 
 export async function handler(data: (typeof Schema)["input"]["_type"]) {
-  const user = (await getUserBySession(data.session)) as User;
-  let { data: queryUserData, error: userError } = await supabase
-    .from("profiles")
-    .select("*")
-    .eq("uid", user.id)
-    .single();
-
-  if (!queryUserData) {
-    return NextResponse.json({ error: "Can't find user" });
-  }
-
-  let { data: codeData } = await supabase
-    .from("invites")
-    .select("*")
-    .eq("code", data.code)
-    .single();
-
-  if (!codeData || (codeData && codeData.used)) {
-    return NextResponse.json({ error: "Can't find code, or used" });
-  }
-
-  if (codeData.type == "clan") {
-    if (queryUserData.clan) {
-      return NextResponse.json({
-        error: "You can't join another clan while being in a clan",
-      });
-    }
-
-    let { data: queryClanData, error: clanError } = await supabase
-      .from("clans")
-      .select("*")
-      .eq("id", Number(codeData.resourceId))
-      .single();
-
-    if (!queryClanData) {
-      return NextResponse.json({
-        error: "No such clan",
-      });
-    }
-
-    await supabase.from("profiles").upsert({
-      id: queryUserData.id,
-      clan: queryClanData?.id,
-    });
-
-    return NextResponse.json({});
-  }
-
-  return NextResponse.json({ error: "Unknown invite type" });
+  return NextResponse.json({ error: "Disabled" }, { status: 403 });
 }
