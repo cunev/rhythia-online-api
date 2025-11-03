cf3106056ed01dc18cdce1f1590b007601fa83c9
cunev <cunevnew@gmail.com>
2025-11-03 03:09:33 +0200

    feat: remove beatmap deletion logic from handler

--- api/deleteBeatmapPage.ts
+++ api/deleteBeatmapPage.ts
@@ -30,48 +30,5 @@ export async function handler({
 }: (typeof Schema)["input"]["_type"]): Promise<
   NextResponse<(typeof Schema)["output"]["_type"]>
 > {
-  const user = (await getUserBySession(session)) as User;
-  let { data: userData, error: userError } = await supabase
-    .from("profiles")
-    .select("*")
-    .eq("uid", user.id)
-    .single();
-
-  let { data: pageData, error: pageError } = await supabase
-    .from("beatmapPages")
-    .select("*")
-    .eq("id", id)
-    .single();
-
-  if (!pageData) return NextResponse.json({ error: "No beatmap." });
-
-  let { data: beatmapData, error: bmPageError } = await supabase
-    .from("beatmaps")
-    .select("*")
-    .eq("beatmapHash", pageData.latestBeatmapHash || "-1-1-1-1")
-    .single();
-
-  if (!userData) return NextResponse.json({ error: "No user." });
-  if (!beatmapData) return NextResponse.json({ error: "No beatmap." });
-
-  if (userData.id !== pageData.owner) {
-    const isDev =
-      (userData.badges as string[]).includes("Developer") ||
-      (userData.badges as string[]).includes("Global Moderator");
-
-    if (!isDev) return NextResponse.json({ error: "Non-authz user." });
-  }
-
-  if (pageData.status !== "UNRANKED")
-    return NextResponse.json({ error: "Only unranked maps can be updated" });
-
-  await supabase.from("beatmapPageComments").delete().eq("beatmapPage", id);
-  await supabase.from("collectionRelations").delete().eq("beatmapPage", id);
-  await supabase.from("beatmapPages").delete().eq("id", id);
-  await supabase
-    .from("beatmaps")
-    .delete()
-    .eq("beatmapHash", beatmapData.beatmapHash);
-
-  return NextResponse.json({});
+  return NextResponse.json({ error: "Can't remove beatmaps" });
 }
